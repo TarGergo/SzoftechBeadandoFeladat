f3635d40231ad24b90b0f796aff34a9c134c7585
TarGergo <92146443+TarGergo@users.noreply.github.com>
2021-11-14 14:27:38 +0100

    Add, delete, edit methods in the viewmodel (not tested yet)

---
+++ Viewmodel/ProductViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FurnitureStoreApp.Model;
+using FurnitureStoreApp.Viewmodel.Base;
+
+namespace FurnitureStoreApp.Viewmodel
+{
+    class ProductViewModel : BaseViewModel
+    {
+        public ObservableCollection<ProductDTO> Products { get; set; }
+        public ProductDTO currentProduct;
+        public DefaultCommand addCommand { get; }
+        public DefaultCommand editCommand { get; }
+        public DefaultCommand deleteCommand { get; }
+        public string infoMessage;
+        private string errorMessage = "Something went wrong! Operation failed!";
+        private ProductService productService;
+
+        public ProductViewModel()
+        {
+            productService = new ProductService();
+            currentProduct = new ProductDTO();
+            loadData();
+            addCommand = new DefaultCommand(add);
+            editCommand = new DefaultCommand(edit);
+            deleteCommand = new DefaultCommand(delete);
+
+        }
+
+        public void loadData()
+        {
+            Products = new ObservableCollection<ProductDTO>(productService.getAllProduct());
+        }
+
+     
+        private void add()
+        {
+            try
+            {
+                bool isAdded = productService.add(currentProduct);
+                loadData();
+                if (isAdded)
+                {
+                    infoMessage = "Product successfully added to the database!";
+                }
+                else
+                {
+                    infoMessage = errorMessage;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void delete()
+        {
+            try
+            {
+                var isDeleted = productService.delete(currentProduct.Id);
+                loadData();
+                if (isDeleted)
+                {
+                    infoMessage = "Product Successfully deleted from the database!";
+                }
+                else
+                {
+                    infoMessage = errorMessage;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        private void edit()
+        {
+            try
+            {
+                var isEdited = productService.edit(currentProduct);
+                loadData();
+                if (isEdited)
+                {
+                    infoMessage = "Product successfully edited!";
+                }
+                else
+                {
+                    infoMessage = errorMessage;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
