06ca472b05814fe6020408dc9ec243e1b5dac188
TarGergo <92146443+TarGergo@users.noreply.github.com>
2021-11-14 13:10:31 +0100

    Importing database + DTO class

---
+++ Model/ProductDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureStoreApp.Model
+{
+    class ProductDTO
+    {
+        public int MyProperty { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public int Height { get; set; }
+    }
+}
